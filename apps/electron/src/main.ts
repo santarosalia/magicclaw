5953ac3ddda1397724389b52dd56a7d56cdb9504
santarosalia <mdc@kakao.com>
2026-02-11 00:11:43 +0900

    찐찐막

--- apps/electron/src/main.ts
+++ apps/electron/src/main.ts
@@ -85,7 +85,13 @@ function startProdServers() {
   // Next Web (web-next/.next, node_modules/next → next start -p 3000)
   // const nodeModulesDir = path.join(resourcesBase, "web-next", "node_modules");
   // const nextBin = path.join(nodeModulesDir, "next", "dist", "bin", "next");
-  const webNextDir = path.join(resourcesBase, "web-next");
+  const webNextDir = path.join(
+    resourcesBase,
+    "web-next",
+    "magicclaw",
+    "apps",
+    "web",
+  );
   const serverJs = path.join(webNextDir, "server.js");
 
   webProcess = spawn(process.execPath, [serverJs], {
